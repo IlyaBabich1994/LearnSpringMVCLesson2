ad604986c964705fbc33c59c2d5b8a7237d66529
IlyaBabich1994 <ktljva@gmail.com>
2023-06-06 12:44:56 +0200

    Init

--- src/main/java/org/growUpToMiddle/controller/UserController.java
+++ src/main/java/org/growUpToMiddle/controller/UserController.java
@@ -1,19 +1,4 @@
 package org.growUpToMiddle.controller;
 
 public class UserController {
-
-    public String getAllUsers() {
-    }
-
-    public String getUserById() {
-    }
-
-    public String addUser() {
-    }
-
-    public String showAddUserForm() {
-    }
-
-    public String deleteUser() {
-    }
 }
